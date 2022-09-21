09431a8acf4096089d865323c6c21ed33ff5e1a2
Allan Latty <allan.latty@gmail.com>
2022-09-20 22:41:05 -0400

    JS file is able to fetch content from the quote api. Button click is responsive. Quotes a delivered on load and after button click.

--- Quote Generator/main.js
+++ Quote Generator/main.js
@@ -1,1 +1,33 @@
 console.log("JS Scaffolding Complete")
+
+document.addEventListener("DOMContentLoaded", () => {
+   
+    const citation = document.getElementById("author")
+    
+    const quote = document.getElementById("quote")
+
+    const button = document.getElementById("button")
+
+    console.log(citation, quote, button)
+
+    async function updateQuotation() {
+        const res = await fetch("https://api.quotable.io/random")
+
+        const data = await res.json()
+
+        if(res.ok) {
+            quote.textContent = data.content
+            citation.textContent = data.author
+        } else {
+            quote.textContent = "An error is present"
+
+            console.log(data)
+        }
+    }
+
+    button.addEventListener('click', updateQuotation)
+
+    updateQuotation()
+
+})
+
